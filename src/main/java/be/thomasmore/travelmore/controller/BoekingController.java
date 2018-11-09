f5266e2e8852df7e4a488c966e1ca71090e46124
Joren Synaeve <jorensynaeve@hotmail.com>
2018-11-09 11:16:36 +0100

    Joren: bevestigingsmail

--- src/main/java/be/thomasmore/travelmore/controller/BoekingController.java
+++ src/main/java/be/thomasmore/travelmore/controller/BoekingController.java
@@ -1,7 +1,46 @@
 package be.thomasmore.travelmore.controller;
 
+import be.thomasmore.travelmore.domain.Reis;
+
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@ViewScoped
+@ManagedBean
 public class BoekingController {
-    public void boekReis() {
+    @Resource(name = "java:jboss/mail/gmail")
+    private Session session;
+
+    public BoekingController() {
+    }
+
+    public String boekReis(int betaalmethode, Reis reis) {
         
+        // Send email
+        sendMail("synaevejoren@gmail.com", "Bevestiging", "U hebt bevestigd!");
+
+        return "boekingBevestiging";
+    }
+
+    private void sendMail(String to, String subject, String body) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+            System.out.println("Mail sent!");
+        } catch (MessagingException e) {
+            System.out.println("Error sending mail!");
+            System.out.println(e);
+        }
     }
 }
