324f8aa628b5d157a81542e05abd2c21664dc827
Mr_olive123 <michiel336@gmail.com>
2018-12-06 19:01:42 +0100

    mail

--- src/main/java/be/thomasmore/travelmore/controller/BoekingController.java
+++ src/main/java/be/thomasmore/travelmore/controller/BoekingController.java
@@ -8,12 +8,10 @@ import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @ViewScoped
 @ManagedBean
@@ -28,6 +26,33 @@ public class BoekingController {
     }
 
     public String boekReis(int betaalmethode, Reis reis) {
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.store.protocol", "pop3");
+        props.put("mail.transport.protocol", "smtp");
+        final String username = "groep4javaproject@gmail.com";//
+        final String password = "JavaProjectGroep4+";
+        try {
+            session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+        }catch (Exception e) {
+            System.out.println("Error sending mail!");
+            System.out.println(e);
+        }
+
+
+
         // Send email
         String to = authService.getKlant().getEmail();
         String body = "Dag " + authService.getKlant().getVoornaam() + "!";
@@ -46,6 +71,8 @@ public class BoekingController {
 
     private void sendMail(String to, String subject, String body) {
         try {
+
+
             Message message = new MimeMessage(session);
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
